beda97ee246019e1538428718c55608a1001e05d
helicorn <kmw4907@gmail.com>
2022-11-28 20:09:27 +0900

    첫 커밋_김민우

---
+++ Hwarang/src/main/java/com/hwarang/HwarangApplication.java
@@ -0,0 +1,13 @@
+package com.hwarang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwarangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwarangApplication.class, args);
+	}
+
+}
