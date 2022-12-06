daee4dc7c660895c056882be03773e10d1b0407e
kim\kim <jiin724@gmail.com>
2022-12-06 23:32:27 +0900

    test

--- Hwarang/src/main/java/com/hwarang/controller/HomeController.java
+++ Hwarang/src/main/java/com/hwarang/controller/HomeController.java
@@ -1,8 +1,23 @@
 package com.hwarang.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController {
-
+	
+	@RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+	
+	@GetMapping("/info")
+	public @ResponseBody String info() {
+		return "개인정보";
+	}
 }
