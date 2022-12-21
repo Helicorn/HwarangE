4206b719d0d599146bb873306685dfadfcbbfa06
DESKTOP-NRSRQ3H\djaak <eoghgha@naver.com>
2022-12-21 19:10:07 +0900

    sql및 vo이름 변경
    _가 들어간 sql문들을 모두 수정

--- Hwarang/src/main/java/com/hwarang/vo/UpboardVo.java
+++ Hwarang/src/main/java/com/hwarang/vo/UpboardVo.java
@@ -7,24 +7,24 @@ import lombok.Setter;
 @Setter
 public class UpboardVo {
 	
-	private int upboard_no; 		/*게시번호*/
-    private String upboard_title;	/*제목*/
-    private String upboard_writer; 	/*작성자*/
-    private String upboard_score; 	/*평점*/
-    private String upboard_cont; 	/*본문내용*/
-    private String upboard_photo1; 	/*사진1*/
-    private String upboard_photo2; 	/*사진2*/
-    private String upboard_photo3; 	/*사진3*/
-    private String upboard_photo4; 	/*사진4*/
-    private String upboard_photo5; 	/*사진5*/
-    private int upboard_view; 		/*조회수*/
-    private String upboard_date; 	/*등록일*/
+	private int upboardNo; 		/*게시번호*/
+    private String upboardTitle;	/*제목*/
+    private String upboardWriter; 	/*작성자*/
+    private String upboardScore; 	/*평점*/
+    private String upboardCont; 	/*본문내용*/
+    private String upboardPhoto1; 	/*사진1*/
+    private String upboardPhoto2; 	/*사진2*/
+    private String upboardPhoto3; 	/*사진3*/
+    private String upboardPhoto4; 	/*사진4*/
+    private String upboardPhoto5; 	/*사진5*/
+    private int upboardView; 		/*조회수*/
+    private String upboardDate; 	/*등록일*/
     
     //페이징 관련변수
   	private int startrow;//시작행 번호
   	private int endrow;//끝행 번호
 
   	//검색어와 검색필드 관련변수
-  	private String find_field;//검색필드
-  	private String find_name;//검색어	
+  	private String findField;//검색필드
+  	private String findName;//검색어	
 }
