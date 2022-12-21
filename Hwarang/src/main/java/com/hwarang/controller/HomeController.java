e3fb582cd938431e6dbfbea58d6c35484388c91d
helicorn <kmw4907@gmail.com>
2022-12-21 19:22:21 +0900

    그래들빌드 수정_김민우

--- Hwarang/src/main/java/com/hwarang/controller/HomeController.java
+++ Hwarang/src/main/java/com/hwarang/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.hwarang.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 import org.springframework.security.core.Authentication;
